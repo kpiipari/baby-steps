ad251aea5d9475d4966a41d96504195a22afdd8d
Kaisa Piipari <kaisa.piipari@gmail.com>
2017-08-23 20:20:42 +0100

    Adds tests to child controller

--- spec/controllers/child_controller_spec.rb
+++ spec/controllers/child_controller_spec.rb
@@ -15,5 +15,57 @@ describe ChildController do
             expect(last_response.body).to include("Add a child")
             
         end
-    end     
+    end 
+    
+    describe 'Childs homepage' do
+        it 'loads a new page for a child' do
+            parent = Parent.create(:username => "daddyo", :email => "daddycool@example.com", :password => "tiger")
+            params = {
+            :username => "daddyo",
+            :password => "tiger"
+            }
+            post '/login', params
+
+            child = Child.create(:name => "Benny", :dob => "15/03/2016")
+            
+            get '/child/:slug'
+
+            expect(last_response.body).to include("Benny")
+            expect(last_response.body).to include("2016-03-15")
+        end
+        it 'tells when there are no milestones' do
+            parent = Parent.create(:username => "daddyo", :email => "daddycool@example.com", :password => "tiger")
+            params = {
+            :username => "daddyo",
+            :password => "tiger"
+            }
+            post '/login', params
+
+            child = Child.create(:name => "Benny", :dob => "15/03/2016")
+            
+            get '/child/:slug'
+
+            expect(last_response.body).to include("You have not added any milestones yet")
+
+        end
+        it 'shows all the milestones associated with a child' do
+
+            parent = Parent.create(:username => "daddyo", :email => "daddycool@example.com", :password => "tiger")
+            params = {
+            :username => "daddyo",
+            :password => "tiger"
+            }
+            post '/login', params
+            
+            child = Child.create(:name => "Benny", :dob => "15/03/2016")
+            milestone = Milestone.create(:content => "first steps", :date => "10-09-2016", :age => "13 months")
+            child.milestones << milestone
+            
+            get '/child/:slug'
+
+            expect(last_response.body).to include("first steps")
+            expect(last_response.body).to include("On 2016-09-10")
+            expect(last_response.body).to include("At 13 months")
+        end
+    end    
 end
